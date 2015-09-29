a1c2ba57908329214b90dea64500c937150458a5
Teddy Wing <git@teddywing.com>
2015-09-29 18:39:57 -0400

    git-stats.sh: Use `--use-mailmap` flag
    Add this flag to our `git log` calls in order to recognise mapped
    contributors and properly calculate their commit word count and commit
    days count.

--- git-stats.sh
+++ git-stats.sh
@@ -33,14 +33,14 @@ echo "$colon_separated_shortlog" | asciigraph  -l 'Commit count' --color
 contributors=$(echo "$colon_separated_shortlog" | cut -d: -f 1)
 
 for name in $contributors; do
-	colon_separated_commit_word_count+=$name:$(git log --pretty='format:%B' --no-merges --author="$name" | wc -w | tr -d '[[:space:]]')$'\n'
+	colon_separated_commit_word_count+=$name:$(git log --pretty='format:%B' --no-merges --author="$name" --use-mailmap | wc -w | tr -d '[[:space:]]')$'\n'
 done
 
 echo -n "$colon_separated_commit_word_count" | asciigraph -l 'Commit message word count' --color --sort=dec
 
 
 while read -r name; do
-	first_commit_date=$(git --no-pager log --reverse --pretty='format:%ad' --author=""$name"" | head -n 1)
+	first_commit_date=$(git --no-pager log --reverse --pretty='format:%ad' --author=""$name"" --use-mailmap | head -n 1)
 	days_since_first_commit=$(ruby -e 'require "date"; date = Date.parse("'$first_commit_date'"); puts (Date.today - date).to_i')
 	colon_separated_days_since_first_commit+=$name:$days_since_first_commit$'\n'
 done <<< "$contributors"
